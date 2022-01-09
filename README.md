082d8b2d86aafc9f7e0cd70ca02db0df28b70229
Hugh Rawlinson <hughr2005@gmail.com>
2022-01-09 14:40:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Library Search
+
+I'm deliberately not mentioning the service or libraries I use here.
+
+I use multiple libraries. Searching them one by one isn't fun. Now, a CLI I can
+use to search them at the same time. Didn't make a website because I assume I
+would have to deal with CORS.
+
+## Installation
+
+I think you can install a
+
+## Usage
+
+Use a comma separated environmental variable to specify which libraries to
+search. Use the first argument to specify the query.
+
+```sh
+LIBRARIES="fls,afls" library-search "a fictional book"
+```
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+#!/usr/bin/env -S node --loader ts-node/esm -r dotenv/config ./src/index.ts
+import "./env.ts";
+import fetch from "node-fetch";
+
+interface EbookType {
+  name: "eBook";
+  id: "ebook";
+}
+
+interface AudioBookType {
+  name: "Audiobook";
+  id: "audiobook";
+}
+
+interface Creator {
+  id: string;
+  role: string;
+  name: string;
+}
+
+interface Publisher {
+  name: string;
+  id: string;
+}
+
+type BookType = EbookType | AudioBookType;
+
+interface Book {
+  id: string;
+  isAvailable: boolean;
+  isHoldable: true;
+  // subjects: Subject[];
+  creators: Creator[];
+  publisher: Publisher;
+  // languages: Language[];
+  type: BookType;
+  title: string;
+}
+
+const Book = {
+  parse: (book: any): Book => {
+    return {
+      isAvailable: book.isAvailable,
+      isHoldable: true,
+      type: book.type,
+      publisher: book.publisher,
+      creators: book.creators,
+      title: book.title,
+      id: book.id,
+    };
+  },
+};
+
+type BookSearchResult = Book & { library: string };
+
+const libraries = process.env.LIBRARIES?.split(",");
+const query = process.argv[1];
+
+libraries?.forEach(async (library: string) => {
+  const url = `https://${library}.overdrive.com/search?format=audiobook-overdrive&query=${encodeURIComponent(
+    query
+  )}`;
+  const result = await (await fetch(url)).text();
+  const dataLine = result
+    .split("\n")
+    .filter((line) => line.includes("window.OverDrive.titleCollection"))[0];
+  const assignmentIndex = dataLine.indexOf("=");
+  const data = JSON.parse(
+    dataLine.slice(assignmentIndex + 1, dataLine.length - 1)
+  );
+  const books: BookSearchResult[] = data.map(Book.parse).map((book: Book) => ({
+    library,
+    link: `https://${library}.overdrive.com/media/${book.id}`,
+    ...book,
+  }));
+  console.log(JSON.stringify(books, null, 2));
+});
